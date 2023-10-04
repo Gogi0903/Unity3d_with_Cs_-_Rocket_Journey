cc4eb9068f5b911f137a78aa601de43038dc739d
Gogi0903 <gogh.jozsef@gmail.com>
2023-10-04 22:22:49 +0200

    moving objects have been added

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period == Mathf.Epsilon) {return;}
+
+        float cycles = Time.time / period;
+        
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave +1f) / 2;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
